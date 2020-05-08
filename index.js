1d0eec87b22f9a6e25bd6516bfce4476f3d85524
Kaan Yagci <kaan.yagci@clique.paris>
2020-05-08 21:51:45 +0200

    [rpi-monitor] - getCPUUsage & hostname implemented

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 var spawn = require('child_process').spawn;
+const os = require('os');
 
+/**
+* Function returns the current temperature of the device.
+* @namespace getTemperature
+* @return {Promise<number>} returns current device temperature on celcius
+**/
 async function getTemperature() {
     return new Promise((resolve, reject) => {
-	temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp']);
-	temp.stdout.on('data', function(data) {
-            resolve(data/1000);
-	});
+        temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp']);
+        temp.stdout.on('data', function(data) {
+                resolve(data/1000);
+        });
     });
 }
 
-getTemperature().then((temp) => {
-    console.log(`Current temperature ${temp}`);
-});
+const hostname = os.hostname();
 
+/**
+* Function returns the used memory percentage.
+* @namespace getMemoryPercentage
+* @param {type} var parameter description
+* @return {double} returns the used memory pourcentage of the device
+**/
+function getMemoryPercentage() {
+    const free = os.freemem();
+    const total = os.totalmem();
+    return ((total - free) / total) * 100;
+}
+
+/**
+* Function returns the cpu usage of the device.
+* @namespace getCPUUsage
+* @return {double[]} returns the CPU usage percentage as a double
+**/
+async function getCPUUsage() {
+    return new Promise((resolve, reject) => {
+        const startCPUIdle = os.cpus().map((core) => core.times.idle);
+        var endCPUIdle;
+        setTimeout(
+            function () {
+                endCPUIdle = os.cpus().map((core) => core.times.idle); 
+                let idleDifference = endCPUIdle.map((value, index) => value - startCPUIdle[index]);
+                idleDifference = idleDifference.map((idle) => (500 - idle)/500);
+                resolve(idleDifference);
+            },
+            500
+        );
+    });
+}
+
+
+module.exports = {
+    freeMemoryPercentage: getMemoryPercentage,
+    hostname: hostname,
+    temperature: getTemperature,
+    cpuPercentage: getCPUUsage
+}
+
+getCPUUsage();
