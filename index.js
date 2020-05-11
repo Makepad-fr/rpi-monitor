bdfaf6acbb71b27f5f0adea5d7af5983fd9e1863
Kaan Yagci <kaan.yagci@clique.paris>
2020-05-11 19:10:12 +0200

    [index] - getDeviceModel function implemented

--- index.js
+++ index.js
@@ -41,6 +41,20 @@ function getMemoryPercentage() {
 }
 
 /**
+ * Function returns the Raspberry Pi device model
+ * @namespace getDeviceModel
+ * @return {string} contains the device model
+ */
+async function getDeviceModel() {
+    return new Promise((resolve, reject) => {
+        let temp = spawn('cat', ['/sys/firmware/devicetree/base/model']);
+        temp.stdout.on('data', function(data) {
+            resolve(data/1000);
+        });
+    });
+}
+
+/**
 * Function returns the cpu usage of the device.
 * @namespace getCPUUsage
 * @return {double[]} returns the CPU usage percentage as a double
@@ -70,5 +84,6 @@ module.exports = {
     hostname: hostname,
     temperature: getTemperature,
     cpuPercentage: getCPUUsage,
-    arch: arch
+    arch: arch,
+    deviceModel: getDeviceModel
 };
