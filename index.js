43100a6d5acd3c594bb78307e08828d96eefa2c2
Kaan Yagci <kaan.yagci@clique.paris>
2020-05-11 19:21:27 +0200

    [index] - getDeviceModel buffer return issue fixed and binary character removed at the end

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function getDeviceModel() {
     return new Promise((resolve, reject) => {
         let temp = spawn('cat', ['/sys/firmware/devicetree/base/model']);
         temp.stdout.on('data', function(data) {
-            resolve(data);
+            resolve(data.toString().replace(/\\x00/g,''));
         });
     });
 }
